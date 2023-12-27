921d82cc03d6597c8ea42fc9b32c6ec81181f3cf
Shrikant Kale <kaleshrikant@live.com>
2023-12-27 13:59:28 +0530

    To have the @Observed support we need to register this aspect

---
+++ parent-service/src/main/java/com/spring/boot/parent/config/ObserverConfig.java
@@ -0,0 +1,16 @@
+package com.spring.boot.parent.config;
+
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class ObserverConfig {
+
+    // To have the @Observed support we need to register this aspect
+    @Bean
+    ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
+        return new ObservedAspect(observationRegistry);
+    }
+}
--- parent-service/src/main/java/com/spring/boot/parent/config/ParentConfig.java
+++ parent-service/src/main/java/com/spring/boot/parent/config/ParentConfig.java
@@ -1,11 +1,11 @@
-package com.spring.boot.child.controller.config;
+package com.spring.boot.parent.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class ChildConfig {
+public class ParentConfig {
 
     @Bean
     public RestTemplate restTemplate(){
